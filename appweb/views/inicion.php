aa786784a8d7ea60019fcbe70dbe2fa79799af45
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2018-09-19 23:36:51 -0400

    ejemplo: ajustado el ejemplo, variable array ajustada a vacio, no warning

--- appweb/views/inicion.php
+++ appweb/views/inicion.php
@@ -6,9 +6,18 @@
 		echo br().PHP_EOL;  // genera neuva linea en codigo html al navegador
 		echo form_fieldset('titulo marco',array('class'=>'containerin ') );
 		$this->table->clear();
-		$this->table->set_datatables( array("topindexpager"=>TRUE) );
+		    // si se quiere data tables se debe pasar algo o definir/invocar "set_datatables"
+		$this->table->set_datatables( array("fixedColumns"=>TRUE) );
+		    // para definir bordes, se debe proveer un template con border=1 o pasar en el mismo una clase/estilo css
+			$this->table->set_template(array ( 'table_open'  => '<table border="1" cellpadding="2" cellspacing="2" class="table">','cell_start' => '<td class="btn-large">' ) );
 			$this->table->set_heading('col1: ','col2');
 			$this->table->add_row('celda y alla campo select autogenerado:', form_dropdown('cod_subcategoria', array('1', '2', 'otr'),null,'id="list_subcategoria"'));
+			$this->table->add_row('celda tabla2: ','otra celsa22');
+			$this->table->add_row('celda tabla3: ','otra celsa23');
+			$this->table->add_row('celda tabla2: ','otra celsa22');
+			$this->table->add_row('celda tabla3: ','otra celsa23');
+			$this->table->add_row('celda tabla2: ','otra celsa22');
+			$this->table->add_row('celda tabla3: ','otra celsa23');
 			$this->table->add_row('celda tabla2: ','otra celsa22');
 			$this->table->add_row('celda tabla3: ','otra celsa23');
 			$this->table->add_row('celda tabla4: ','otra celsa24');
