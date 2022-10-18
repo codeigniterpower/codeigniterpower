537ec86c29328b487729701ba17d7072975410d9
general <mckaygerhard@gmail.com>
2022-10-18 11:28:38 -0400

    Fixed a bug in library loader that ignored attempts for (re)loading databases instances
    * when we do `get_instance()->db` even when the old database connection is dead.
    * backported https://github.com/bcit-ci/CodeIgniter/commit/9d0ab04e1e39bc93c59f60844dd2cf9176443028

--- appsys/core/Loader.php
+++ appsys/core/Loader.php
@@ -328,7 +328,7 @@ class CI_Loader {
 		$CI =& get_instance();
 
 		// Do we even need to load the database class?
-		if (class_exists('CI_DB') AND $return == FALSE AND $active_record == NULL AND isset($CI->db) AND is_object($CI->db))
+		if ($return == FALSE AND $active_record == NULL AND isset($CI->db) AND is_object($CI->db))
 		{
 			return FALSE;
 		}
